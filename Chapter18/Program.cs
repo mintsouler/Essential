dc7830cab1b73b99311828cf8b5a09ddf32f9cec
liqinzhao <liqinzhao@eims.com.cn>
2020-07-26 16:55:27 +0800

    begin

---
+++ Chapter18/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Chapter18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
