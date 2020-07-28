35a0ae36d24e0c66c1d05cd8fb820edb5ca9961e
liqinzhao <liqinzhao@eims.com.cn>
2020-07-28 20:50:44 +0800

    add

--- Chapter18/Program.cs
+++ Chapter18/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
 namespace Chapter18
 {
@@ -7,6 +9,53 @@ namespace Chapter18
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            //GetProperty();
+
+            Generic();
+        }
+
+        public static void GetProperty()
+        {
+            DateTime dateTime = new DateTime();
+            Type type = dateTime.GetType();
+            Console.WriteLine("------------属性-----------------");
+            foreach (System.Reflection.PropertyInfo property in type.GetProperties())
+            {
+                Console.WriteLine(property.Name);
+                //property.Attributes
+                property.get
+            }
+
+            Console.WriteLine("------------特性-----------------");
+            foreach (var attr in type.GetCustomAttributesData())
+            {
+                Console.WriteLine(attr.ToString());
+            }
+
+            Console.WriteLine("------------方法-----------------");
+            foreach (System.Reflection.MethodInfo method in type.GetMethods())
+            {
+                Console.WriteLine(method.Name);
+            }
+
+            //调用
+            //Console.WriteLine(type.GetMethod("").Invoke());
+
+
+
+        }
+
+        public static void Generic()
+        {
+            Console.WriteLine("\n泛型参数的类型：");
+            Dictionary<string, int> dic = new Dictionary<string, int>();
+
+            Type t = dic.GetType();
+            foreach (Type type in t.GetGenericArguments())
+            {
+                Console.WriteLine(type.FullName);
+            }
         }
     }
 }
